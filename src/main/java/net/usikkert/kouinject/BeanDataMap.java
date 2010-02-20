b01f9eae1f28a9180769a1441e96a514cfd068c4
Christian Ihle <blurpy@gmail.com>
2010-02-20 20:19:06 +0000

    Added class for the bean-data map. not in use yet

---
+++ src/main/java/net/usikkert/kouinject/BeanDataMap.java
@@ -0,0 +1,130 @@
+
+/***************************************************************************
+ *   Copyright 2009 by Christian Ihle                                      *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This file is part of KouInject.                                       *
+ *                                                                         *
+ *   KouInject is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouInject is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouInject.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouinject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import net.usikkert.kouinject.beandata.BeanData;
+import net.usikkert.kouinject.beandata.Dependency;
+import net.usikkert.kouinject.util.Validate;
+
+/**
+ * Map with bean-data for all the beans possible to create.
+ *
+ * @author Christian Ihle
+ */
+public class BeanDataMap {
+
+    private final Map<Dependency, BeanData> beanDataMap;
+
+    /**
+     * Creates a new instance of this bean-data map, ready to use.
+     */
+    public BeanDataMap() {
+        this.beanDataMap = new HashMap<Dependency, BeanData>();
+    }
+
+    /**
+     * Adds bean-data to the map.
+     *
+     * @param description The description of the bean-data.
+     * @param beanData The bean-data to add.
+     */
+    public synchronized void addBeanData(final Dependency description, final BeanData beanData) {
+        Validate.notNull(description, "Description can not be null");
+        Validate.notNull(beanData, "BeanData can not be null");
+
+        if (containsBeanData(description)) {
+            throw new IllegalArgumentException("Cannot add already existing bean-data: " + description);
+        }
+
+        beanDataMap.put(description, beanData);
+    }
+
+    /**
+     * Gets bean-data from the map.
+     *
+     * @param beanNeeded The description of the bean-data to get.
+     * @return The bean-data, or {@link IllegalArgumentException} if not found.
+     */
+    public synchronized BeanData getBeanData(final Dependency beanNeeded) {
+        return getBeanData(beanNeeded, true);
+    }
+
+    private BeanData getBeanData(final Dependency beanNeeded, final boolean throwEx) {
+        Validate.notNull(beanNeeded, "Bean needed can not be null");
+
+        final List<Dependency> matches = new ArrayList<Dependency>();
+        final Iterator<Dependency> iterator = beanDataMap.keySet().iterator();
+
+        while (iterator.hasNext()) {
+            final Dependency bean = iterator.next();
+
+            if (beanNeeded.canInject(bean)) {
+                matches.add(bean);
+            }
+        }
+
+        if (matches.size() == 0) {
+            if (throwEx) {
+                throw new IllegalArgumentException("No matching bean-data found for " + beanNeeded);
+            }
+
+            else {
+                return null;
+            }
+        }
+
+        else if (matches.size() > 1) {
+            throw new IllegalStateException("Too many matching bean-data found for " + beanNeeded + " " + matches);
+        }
+
+        return beanDataMap.get(matches.get(0));
+    }
+
+    /**
+     * Returns the size of the bean-data map.
+     *
+     * @return The size of the bean-data map.
+     */
+    public synchronized int size() {
+        return beanDataMap.size();
+    }
+
+    /**
+     * Checks if the bean-data exists in the map.
+     *
+     * @param description The description of the bean-data.
+     * @return If the bean-data exists in the map.
+     */
+    public synchronized boolean containsBeanData(final Dependency description) {
+        Validate.notNull(description, "Description can not be null");
+
+        final BeanData beanData = getBeanData(description, false);
+        return beanData != null;
+    }
+}
---
+++ src/test/java/net/usikkert/kouinject/BeanDataMapTest.java
@@ -0,0 +1,164 @@
+
+/***************************************************************************
+ *   Copyright 2009 by Christian Ihle                                      *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This file is part of KouInject.                                       *
+ *                                                                         *
+ *   KouInject is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouInject is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouInject.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouinject;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import net.usikkert.kouinject.beandata.BeanData;
+import net.usikkert.kouinject.beandata.Dependency;
+import net.usikkert.kouinject.beandata.InjectionPoint;
+import net.usikkert.kouinject.testbeans.scanned.CarBean;
+import net.usikkert.kouinject.testbeans.scanned.FieldBean;
+import net.usikkert.kouinject.testbeans.scanned.HelloBean;
+import net.usikkert.kouinject.testbeans.scanned.SetterBean;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test of {@link BeanDataMap}.
+ *
+ * @author Christian Ihle
+ */
+public class BeanDataMapTest {
+
+    private BeanDataMap map;
+
+    @Before
+    public void createBeanDataMap() {
+        map = new BeanDataMap();
+    }
+
+    @Test
+    public void addBeanDataShouldPutTheBeanInTheMap() {
+        assertEquals(0, map.size());
+
+        final BeanData beanData = createBeanData(Object.class);
+        final Dependency bean = new Dependency(Object.class);
+
+        map.addBeanData(bean, beanData);
+
+        assertEquals(1, map.size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addBeanDataShouldFailToAddTheSameBeanTwice() {
+        assertEquals(0, map.size());
+
+        final BeanData beanData = createBeanData(Object.class);
+        final Dependency bean = new Dependency(Object.class);
+
+        map.addBeanData(bean, beanData);
+        map.addBeanData(bean, beanData);
+    }
+
+    @Test
+    public void addBeanDataShouldAcceptSeveralDifferentBeans() {
+        assertEquals(0, map.size());
+
+        final BeanData setterBean = createBeanData(SetterBean.class);
+        final Dependency setterBeanDependency = new Dependency(SetterBean.class);
+
+        map.addBeanData(setterBeanDependency, setterBean);
+        assertEquals(1, map.size());
+
+        final BeanData fieldBean = createBeanData(FieldBean.class);
+        final Dependency fieldBeanDependency = new Dependency(FieldBean.class);
+
+        map.addBeanData(fieldBeanDependency, fieldBean);
+        assertEquals(2, map.size());
+
+        final BeanData helloBean = createBeanData(HelloBean.class);
+        final Dependency helloBeanDependency = new Dependency(HelloBean.class);
+
+        map.addBeanData(helloBeanDependency, helloBean);
+        assertEquals(3, map.size());
+    }
+
+    @Test
+    public void getBeanDataShouldFindTheCorrectBeanData() {
+        addTestBeans();
+
+        final BeanData setterBean = map.getBeanData(new Dependency(SetterBean.class));
+        assertNotNull(setterBean);
+        assertTrue(setterBean.getBeanClass().equals(SetterBean.class));
+
+        final BeanData fieldBean = map.getBeanData(new Dependency(FieldBean.class));
+        assertNotNull(fieldBean);
+        assertTrue(fieldBean.getBeanClass().equals(FieldBean.class));
+
+        final BeanData helloBean = map.getBeanData(new Dependency(HelloBean.class));
+        assertNotNull(helloBean);
+        assertTrue(helloBean.getBeanClass().equals(HelloBean.class));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getBeanDataShouldFailIfNoMatchFound() {
+        addTestBeans();
+
+        map.getBeanData(new Dependency(CarBean.class));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void getBeanDataShouldFailIfTooManyMatches() {
+        addTestBeans();
+
+        map.getBeanData(new Dependency(Object.class));
+    }
+
+    @Test
+    public void containsBeanDataShouldDetectAddedBeanData() {
+        addTestBeans();
+
+        assertTrue(map.containsBeanData(new Dependency(SetterBean.class)));
+        assertTrue(map.containsBeanData(new Dependency(FieldBean.class)));
+        assertTrue(map.containsBeanData(new Dependency(HelloBean.class)));
+    }
+
+    @Test
+    public void containsBeanDataShouldHandleMissingBeanData() {
+        addTestBeans();
+
+        assertFalse(map.containsBeanData(new Dependency(CarBean.class)));
+    }
+
+    private void addTestBeans() {
+        final BeanData setterBean = createBeanData(SetterBean.class);
+        final Dependency setterBeanDependency = new Dependency(SetterBean.class);
+        map.addBeanData(setterBeanDependency, setterBean);
+
+        final BeanData fieldBean = createBeanData(FieldBean.class);
+        final Dependency fieldBeanDependency = new Dependency(FieldBean.class);
+        map.addBeanData(fieldBeanDependency, fieldBean);
+
+        final BeanData helloBean = createBeanData(HelloBean.class);
+        final Dependency helloBeanDependency = new Dependency(HelloBean.class);
+        map.addBeanData(helloBeanDependency, helloBean);
+    }
+
+    private BeanData createBeanData(final Class<?> beanClass) {
+        return new BeanData(beanClass, null, new ArrayList<InjectionPoint>(), false);
+    }
+}
