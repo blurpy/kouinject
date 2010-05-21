a94e35fd25f42a14c0ffe96cc00ff369c00ad60b
Christian Ihle <blurpy@gmail.com>
2010-05-21 16:12:26 +0000

    Updated copyright in header to include 2010

---
+++ header-updater.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Updates the third line in all java files.
+
+for file in $(find . -name "*.java"); do
+	sed -i -e '3 s/*   Copyright 2009 by Christian Ihle                                      */*   Copyright 2009-2010 by Christian Ihle                                 /' $file
+done
