cfdc47e4de689f44bcc1a61b74381ea48ecac47a
Christian Ihle <blurpy@gmail.com>
2011-03-03 16:59:28 +0000

    Moving header update script to config folder

--- header-updater.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Updates the third line in all java files.
-
-for file in $(find . -name "*.java"); do
-	sed -i -e '3 s/*   Copyright 2009-2010 by Christian Ihle                                 */*   Copyright 2009-2011 by Christian Ihle                                 /' $file
-done
