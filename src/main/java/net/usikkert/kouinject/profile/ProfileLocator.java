618c3267eceda968896fe34181ee53c077cef1c1
Christian Ihle <blurpy@gmail.com>
2011-03-06 11:02:09 +0000

    Adding interface for locating active profiles

---
+++ src/main/java/net/usikkert/kouinject/profile/ProfileLocator.java
@@ -0,0 +1,40 @@
+
+/***************************************************************************
+ *   Copyright 2009-2011 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This file is part of KouInject.                                       *
+ *                                                                         *
+ *   KouInject is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouInject is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouInject.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouinject.profile;
+
+import java.util.List;
+
+/**
+ * Interface for locating the active profiles to use for deciding which beans that can be loaded.
+ *
+ * @author Christian Ihle
+ */
+public interface ProfileLocator {
+
+    /**
+     * Gets a list of all the currently active profiles in the injector,
+     *
+     * @return All active profiles.
+     */
+    List<String> getActiveProfiles();
+}
