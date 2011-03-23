c70e766fba23fcd23af06911306e34c73f2e2e1d
Christian Ihle <blurpy@gmail.com>
2011-03-23 17:48:22 +0000

    Adding testbeans for generics on subclasses, deactivated until implemented

---
+++ src/test/java/net/usikkert/kouinject/testbeans/scanned/generics/thing/FirstStartThingListenerBean.java
@@ -0,0 +1,33 @@
+
+/***************************************************************************
+ *   Copyright 2009-2011 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This file is part of KouInject.                                       *
+ *                                                                         *
+ *   KouInject is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouInject is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouInject.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouinject.testbeans.scanned.generics.thing;
+
+/**
+ * A bean for testing generics, inheriting from an abstract class that uses a generic interface.
+ *
+ * @author Christian Ihle
+ */
+//@Component // TODO
+public class FirstStartThingListenerBean extends AbstractStartThingListenerBean {
+
+}
